c83deb6eb671eeed44ac0e7ab9cba9d855a89394
amith01994 <amith01994@gmail.com>
2018-04-21 19:09:48 +0530

    adding more files

---
+++ src/FactoryRunner.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class FactoryRunner{
+
+
+  public static void main(String[] args){
+      HashMap hashmap = new HashMap();
+      System.out.println("Testing");
+    
+
+
+
+  }
+}
